2a9fa8c7e2e436231870a4f2869a7ba0fbaeea0d
whisper <whisper@whisper>
2017-10-09 18:03:45 +0800

    修改添加点评用户搜索逻辑（取消真实用户） 添加返回品种品牌返回数据的企业电话数据

--- model/Action/searchUser.php
+++ model/Action/searchUser.php
@@ -3,7 +3,7 @@
 require '../../common.php';
 include '../../wxAction.php';
 $q = str_replace('"', '', strtolower($_GET ["term"]));
-$result = $db->query("select * from WXUser where UserName like '%$q%' limit 0,10");
+$result = $db->query("select * from WXUser where UserName like '%$q%' and UserUnionId ='0' or ISNULL(UserUnionId)  limit 0,10");
 foreach ($result as $row) {
     $data [] = array(
         'value' => del0($row ['UserId']) . ";" . $row ['UserName'],
--- wxApi/getBrandByCropId.php
+++ wxApi/getBrandByCropId.php
@@ -3,7 +3,7 @@
 require '../common.php';
 include '../wxAction.php';
 $wxCropId = $_GET['CropId'];
-$sql = "select a.id,c.BrandName,d.EnterpriseName,c.BrandImg,d.EnterpriseId from WXCropBrand a
+$sql = "select a.id,c.BrandName,d.EnterpriseName,d.EnterpriseTelephone,c.BrandImg,d.EnterpriseId from WXCropBrand a
 left join WXCrop b on a.CropId=b.CropId
 left join AppBrand c ON a.BrandId=c.BrandId
 left join AppEnterprise d on c.BrandCompany=d.EnterpriseId
